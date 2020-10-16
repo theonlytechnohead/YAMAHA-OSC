4f43cc3ee0344afd5d00d9ccd22d7317baa6ca35
Craig Anderson <cranderson0110@gmail.com>
2020-10-16 22:52:30 +1300

    Refactored to remove useless junk
    Thanks Google for autogen-ing that mess for me to clean up... yeesh

--- app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
+++ app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
@@ -1,91 +1,16 @@
 package net.ddns.anderserver.yamahaosc;
 
-import android.annotation.SuppressLint;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
-
-import java.net.*;
-import java.util.*;
-import com.illposed.osc.*;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * Some older devices needs a small delay between UI widget updates
-	 * and a change of the status and navigation bar.
-	 */
-	private static final int UI_ANIMATION_DELAY = 300;
-	private final Handler mHideHandler = new Handler();
-	private View mContentView;
-	private final Runnable mHidePart2Runnable = new Runnable() {
-		@SuppressLint("InlinedApi")
-		@Override
-		public void run() {
-			// Delayed removal of status and navigation bar
-
-			// Note that some of these constants are new as of API 16 (Jelly Bean)
-			// and API 19 (KitKat). It is safe to use them, as they are inlined
-			// at compile-time and do nothing on earlier devices.
-			mContentView.setSystemUiVisibility(
-					View.SYSTEM_UI_FLAG_LOW_PROFILE
-							| View.SYSTEM_UI_FLAG_FULLSCREEN
-							| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-		}
-	};
-	private final Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			hide();
-		}
-	};
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			switch (motionEvent.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					if (AUTO_HIDE) {
-						delayedHide(AUTO_HIDE_DELAY_MILLIS);
-					}
-					break;
-				case MotionEvent.ACTION_UP:
-					view.performClick();
-					break;
-				default:
-					break;
-			}
-			return false;
-		}
-	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -93,41 +18,19 @@ public class FullscreenActivity extends AppCompatActivity {
 
 		setContentView(R.layout.activity_fullscreen);
 
-		mContentView = findViewById(R.id.fullscreen_frame);
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(0);
-		//startActivity(new Intent(this, Mix1Activity.class));
-	}
-
-	private void hide() {
-		// Hide UI first
+		View frameLayout = findViewById(R.id.fullscreen_frame);
 		ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
 			actionBar.hide();
 		}
-
-		// Schedule a runnable to remove the status and navigation bar after a delay
-		mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+		frameLayout.setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LOW_PROFILE
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		//startActivity(new Intent(this, Mix1Activity.class));
 	}
 
-	/**
-	 * Schedules a call to hide() in delay milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
 }
