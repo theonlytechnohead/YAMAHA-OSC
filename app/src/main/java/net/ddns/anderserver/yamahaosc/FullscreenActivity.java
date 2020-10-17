4c072db70a2a2ff2ecbbb497506c08e525715c70
Craig Anderson <cranderson0110@gmail.com>
2020-10-17 20:18:40 +1300

    Fader! Vertical! Thanks, good GitHub library!
    Also, the layout isn't totally broken (yet)
    That's not to say it hasn't been incredibly frustrating still, but.... it works so far

--- app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
+++ app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
@@ -1,10 +1,19 @@
 package net.ddns.anderserver.yamahaosc;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
+import com.lukelorusso.verticalseekbar.VerticalSeekBar;
+
+import java.io.Console;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,6 +27,7 @@ public class FullscreenActivity extends AppCompatActivity {
 
 		setContentView(R.layout.activity_fullscreen);
 
+		// Making it fullscreen...
 		View frameLayout = findViewById(R.id.fullscreen_frame);
 		ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
@@ -30,7 +40,29 @@ public class FullscreenActivity extends AppCompatActivity {
 						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		// Fullscreen done!
+		Button mix1_button = findViewById(R.id.mix1_button);
+		mix1_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//FullscreenActivity.this.startActivity(new Intent(FullscreenActivity.this, Mix1Activity.class));
+			}
+		});
+
+		VerticalSeekBar fader1 = findViewById(R.id.fader1);
+		fader1.setClickToSetProgress(true);
+		fader1.setOnProgressChangeListener(new Function1<Integer, Unit>() {
+			@Override
+			public Unit invoke(Integer integer) {
+				SendOSCFaderValue(integer);
+				return null;
+			}
+		});
 		//startActivity(new Intent(this, Mix1Activity.class));
+	}
+
+	public void SendOSCFaderValue(int faderValue) {
+		Log.d("Fader", "1: " + faderValue);
 	}
 
 }
--- app/src/main/java/net/ddns/anderserver/yamahaosc/Mix1Activity.java
+++ app/src/main/java/net/ddns/anderserver/yamahaosc/Mix1Activity.java
@@ -1,6 +1,7 @@
 package net.ddns.anderserver.yamahaosc;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,19 @@ public class Mix1Activity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mix1);
+		// Making it fullscreen...
+		View frameLayout = findViewById(R.id.mix1_layout);
 		ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
 			actionBar.hide();
 		}
+		frameLayout.setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LOW_PROFILE
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		// Fullscreen done!
 	}
 }
