2e57c2e57f5292db27795220a49c6beab49ba700
Craig Anderson <cranderson0110@gmail.com>
2020-10-17 21:35:32 +1300

    Actual first implementation done!
    Yay it works!

--- app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
+++ app/src/main/java/net/ddns/anderserver/yamahaosc/FullscreenActivity.java
@@ -3,27 +3,23 @@ package net.ddns.anderserver.yamahaosc;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.illposed.osc.OSCMessage;
-import com.illposed.osc.OSCPacket;
 import com.illposed.osc.OSCSerializeException;
+import com.illposed.osc.transport.udp.OSCPortIn;
 import com.illposed.osc.transport.udp.OSCPortOut;
 import com.lukelorusso.verticalseekbar.VerticalSeekBar;
 
-import java.io.Console;
 import java.io.IOException;
-import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -36,9 +32,10 @@ import static java.lang.Thread.sleep;
  */
 public class FullscreenActivity extends AppCompatActivity {
 
-	private String myIP = "192.168.1.50";
-	private int myPort = 12345;
+	private String oscIP = "192.168.1.50";
+	private int oscSendPort = 8000;
 	OSCPortOut oscPortOut;
+	OSCPortIn oscPortIn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +62,7 @@ public class FullscreenActivity extends AppCompatActivity {
 		mix1_button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//FullscreenActivity.this.startActivity(new Intent(FullscreenActivity.this, Mix1Activity.class));
+				// idk implement later
 			}
 		});
 
@@ -82,137 +79,39 @@ public class FullscreenActivity extends AppCompatActivity {
 				return null;
 			}
 		});
-		//startActivity(new Intent(this, Mix1Activity.class));
-
-
-
-		oscThread.start();
-		//Toast.makeText(getApplicationContext(), "Starting...", Toast.LENGTH_SHORT).show();
 	}
 
 	public void SendOSCFaderValue(int faderValue) throws IOException, OSCSerializeException {
-		//android.os.Debug.waitForDebugger();
-		//Log.d("Fader", "1: " + faderValue);
-		ArrayList<Object> moreThingsToSend = new ArrayList<Object>();
-		moreThingsToSend.add("Hello World2");
-		moreThingsToSend.add(123456);
-		moreThingsToSend.add(12.345);
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Handler handler = new Handler(Looper.getMainLooper());
 
-		OSCMessage message2 = new OSCMessage("/mix1/fader1", moreThingsToSend);
-		if (oscPortOut != null) {
-			try {
-				oscPortOut.send(message2);
-				Log.d("OSC", "Sent message!");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+				try {
+					oscPortOut = new OSCPortOut(InetAddress.getByName(oscIP), oscSendPort);
+				} catch(Exception e) {
+					handler.post(() -> Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show());
+					return;
+				}
 
-
-
-	private Thread oscThread = new Thread() {
-		@Override
-		public void run() {
-			/* The first part of the run() method initializes the OSCPortOut for sending messages.
-			 *
-			 * For more advanced apps, where you want to change the address during runtime, you will want
-			 * to have this section in a different thread, but since we won't be changing addresses here,
-			 * we only have to initialize the address once.
-			 */
-			Handler handler = new Handler(Looper.getMainLooper());
-
-			try {
-				// Connect to some IP address and port
-				oscPortOut = new OSCPortOut(InetAddress.getByName(myIP), myPort);
-			} catch(UnknownHostException e) {
-				// Error handling when your IP isn't found
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						Toast.makeText(getApplicationContext(), "UnknownHostException", Toast.LENGTH_SHORT).show();
-					}
-				});
-				return;
-			} catch(Exception e) {
-				// Error handling for any other errors
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-					}
-				});
-				return;
-			}
-
-
-			/* The second part of the run() method loops infinitely and sends messages every 500
-			 * milliseconds.
-			 */
-			while (true) {
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						Toast.makeText(getApplicationContext(), "Looped", Toast.LENGTH_SHORT).show();
-					}
-				});
 				if (oscPortOut != null) {
-					// Creating the message
-					Object[] thingsToSend = new Object[3];
-					thingsToSend[0] = "Hello World";
-					thingsToSend[1] = 12345;
-					thingsToSend[2] = 1.2345;
-
-					/* The version of JavaOSC from the Maven Repository is slightly different from the one
-					 * from the download link on the main website at the time of writing this tutorial.
-					 *
-					 * The Maven Repository version (used here), takes a Collection, which is why we need
-					 * Arrays.asList(thingsToSend).
-					 *
-					 * If you're using the downloadable version for some reason, you should switch the
-					 * commented and uncommented lines for message below
-					 */
-					OSCMessage message = new OSCMessage("/mix1/fader1", Arrays.asList(thingsToSend));
-					// OSCMessage message = new OSCMessage(myIP, thingsToSend);
-
-
-					/* NOTE: Since this version of JavaOSC uses Collections, we can actually use ArrayLists,
-					 * or any other class that implements the Collection interface. The following code is
-					 * valid for this version.
-					 *
-					 * The benefit of using an ArrayList is that you don't have to know how much information
-					 * you are sending ahead of time. You can add things to the end of an ArrayList, but not
-					 * to an Array.
-					 *
-					 * If you want to use this code with the downloadable version, you should switch the
-					 * commented and uncommented lines for message2
-					 */
-					ArrayList<Object> moreThingsToSend = new ArrayList<Object>();
-					moreThingsToSend.add("Hello World2");
-					moreThingsToSend.add(123456);
-					moreThingsToSend.add(12.345);
-
-					OSCMessage message2 = new OSCMessage("/mix1/fader1", moreThingsToSend);
-					//OSCMessage message2 = new OSCMessage(myIP, moreThingsToSend.toArray());
+					ArrayList<Object> arguments = new ArrayList<>();
+					arguments.add(faderValue);
+					handler.post(() -> {
+						TextView textView = findViewById(R.id.db_text);
+						textView.setText(Integer.toString(faderValue));
+					});
+					OSCMessage message = new OSCMessage("/mix1/fader1", arguments);
 
 					try {
-						// Send the messages
 						oscPortOut.send(message);
-						oscPortOut.send(message2);
-						handler.post(new Runnable() {
-							@Override
-							public void run() {
-								Toast.makeText(getApplicationContext(), "OSC!?", Toast.LENGTH_SHORT).show();
-							}
-						});
-						// Pause for half a second
-						sleep(500);
 					} catch (Exception e) {
 						// Error handling for some error
 					}
 				}
 			}
-		}
-	};
+		});
+		t.start();
+	}
 
 }
