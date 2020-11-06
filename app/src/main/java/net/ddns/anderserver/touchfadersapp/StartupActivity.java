4a31487e68f8174f7cb19eecac2dc3f68e181107
Craig Anderson <cranderson0110@gmail.com>
2020-11-06 20:02:58 +1300

    IP addresses can now be input!
    Doesn't do anything though hehe

--- app/src/main/java/net/ddns/anderserver/touchfadersapp/StartupActivity.java
+++ app/src/main/java/net/ddns/anderserver/touchfadersapp/StartupActivity.java
@@ -1,9 +1,15 @@
 package net.ddns.anderserver.touchfadersapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -32,8 +38,51 @@ public class StartupActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         // Fullscreen done!
 
-        Button startButton = findViewById(R.id.button);
+        Button startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener((view) -> startActivity(new Intent(this, FullscreenActivity.class)));
+
+        EditText ipEditText = findViewById(R.id.ipEditText);
+        /*
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       android.text.Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart)
+                            + source.subSequence(start, end)
+                            + destTxt.substring(dend);
+                    if (!resultingTxt
+                            .matches("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i = 0; i < splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+
+        };
+         */
+        ipEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                View view = this.getCurrentFocus();
+                if(view!= null){
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(startButton::performClick);
+                return true;
+            }
+            return false;
+        });
 
     }
 }
